e4d63107a565909378aaa7f0afbe5d1adb969b56
Dan Brady <dannymb360@gmail.com>
2023-03-28 12:36:32 -0400

    eatery

--- scripts/HolidayRoad.js
+++ scripts/HolidayRoad.js
@@ -1,7 +1,9 @@
 import { attractionDropDown } from "./attractions/AttractionProvider.js"
+import { eateriesDropDown } from "./eateries/EateryProvider.js"
 
 export const HolidayRoad = () => {
     return `Holiday World 
-    ${attractionDropDown()}`
+    ${attractionDropDown()}
+    ${eateriesDropDown()}`
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 import { HolidayRoad } from "./HolidayRoad.js"
 import { fetchAttractions } from "./attractions/AttractionDataAccess.js"
+import { fetchEateries } from "./eateries/EateryDataAccess.js"
 
 const mainContainer = document.querySelector("#container")
 
 const render = () => {
     fetchAttractions()
+    .then(()=>fetchEateries())
     .then(
         ()=> {
     mainContainer.innerHTML = HolidayRoad()
